0ca68d3748a1a888cf5ec5dc771ed055ffd86d77
Gildo Alves <104049719+16b8f3@users.noreply.github.com>
2022-05-16 20:09:51 -0300

    js do efeito Maq. de escrever

--- js/javaScript.js
+++ js/javaScript.js
@@ -3,12 +3,29 @@ var divHelloWorld = document.querySelector(".helloWorld");
 var divBalao = document.querySelector(".balao");
 var divBalao2 = document.querySelector(".balao2");
 var numeroSorteado = sorteadoNumero();
+var numeroSorteadoParaFala = sorteadoNumeroParaFala();
+const titulo = document.querySelector('.o_titulo');
+var falaEsquerdaAlice = document.querySelector(".falaDeAlice");
 
 function sorteadoNumero() {
   return Math.floor(Math.random() * 1001);
 }
 
-console.log('O numero sorteado foi: ' + sorteadoNumero());
+function sorteadoNumeroParaFala() {
+  return Math.floor(Math.random() * 5);
+}
+
+function typeWriter(elemento){
+	const textoArray = elemento.innerHTML.split('');
+	elemento.innerHTML = '';
+	textoArray.forEach((letra, i) => {
+		setTimeout(function() {
+			elemento.innerHTML += letra;
+		}, 75 * i);
+	})
+}
+
+console.log('O numero sorteado foi: ' + numeroSorteado);
 
 if(numeroSorteado == 1000){
 	imgAlice.addEventListener("mouseover", function() {
@@ -23,6 +40,16 @@ if(numeroSorteado == 1000){
 }else{
 	console.log('Quando o numero sorteado for 1000 você vai ter uma surpresa');
 
+	sorteadoNumeroParaFala();
+
+	if(numeroSorteadoParaFala == 2){
+		falaEsquerdaAlice.innerHTML = 'I want an <i>elegant apron</i>';
+	}
+	if(numeroSorteadoParaFala == 3){
+		falaEsquerdaAlice.innerHTML = 'エレガントなエプロンが欲しい';
+	}
+
+
 	imgAlice.addEventListener("mouseover", function() {
 		divHelloWorld.style.display = 'none';
 		divBalao.style.display = 'block';
@@ -33,3 +60,5 @@ if(numeroSorteado == 1000){
 		divBalao.style.display = 'none';
 	})
 }
+
+typeWriter(titulo);
